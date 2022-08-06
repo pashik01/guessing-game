ab8e25088a5a3f739979dd6896e935b42768e1b9
pashik01 <105845661+pashik01@users.noreply.github.com>
2022-08-06 16:15:52 +0300

    feat:upd code

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,22 +2,18 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-      this.min = min
-      this.max = max
+      this.min = min;
+      this.max = max;
     }
-
     guess() {
-      this.num = Math.round((this.min + this.max) / 2)
-      return this.num
+      this.num = Math.round((this.min + this.max) / 2);
+      return this.num;
     }
-
     lower() {
-      this.max = this.num
+      this.max = this.num;
     }
-
     greater() {
-      this.min = this.num
+      this.min = this.num;
     }
 }
-
 module.exports = GuessingGame;
